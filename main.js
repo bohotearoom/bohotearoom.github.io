78f67279ebdd4ab7a4fba822cbc345034630975a
Jani <Jani@Janis-MacBook-Pro.local>
2015-09-27 12:25:13 +0100

    all new

--- main.js
+++ main.js
@@ -7,21 +7,6 @@ $(document).ready(function() {
 		$(this).parent().parent().toggleClass('active');
 	});
 	if ($('body').hasClass('contact__page')){
-		initialize();
-		function initialize() {
-			var myLatlng = new google.maps.LatLng(51.368383,-0.446294);
-			var mapOptions = {
-			  center: myLatlng,
-			  zoom: 17
-			};
-			var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-			var marker = new google.maps.Marker({
-			      position: myLatlng,
-			      map: map,
-			      title: 'Boho Tea room & coffee shop'
-			});
-		}
-		google.maps.event.addDomListener(window, 'load', initialize);
 	} else {
 		$('.hero').slick({
 			arrows:false,
